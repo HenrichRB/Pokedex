e34b51e17af4d1a01397377cef4efb42bbf55d86
Henrique Ribeiro <henrichrb@gmail.com>
2023-09-06 01:55:03 -0300

    feat: decreasing verbosity using map

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,5 @@
 
-
-const offset = 0;
-const limit = 10;
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
-
-function convertPokemonToHtml(pokemon) {
+function convertPokemonToLi(pokemon) {
     return `
         <li class="pokemon">
                     <span class="number">#001</span>
@@ -17,19 +12,13 @@ function convertPokemonToHtml(pokemon) {
 
                         <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
                     </div>
-                </li>
+        </li>
     `
-};
+}
 
 const pokemonList = document.getElementById('pokemonList');
 
 pokeApi.getPokemons()
-    .then((pokemons) => {
-        const listItens = []
-
-        for (let i = 0; i < pokemons.length; i++) {
-            const pokemon = pokemons[i];
-            listItens.push(convertPokemonToHtml(pokemon))
-        }
+    .then((pokemons = []) => {
+        pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('')
     })
-
