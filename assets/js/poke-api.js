b010e123a5010ad60c75f8312efe0d4c068c467f
Henrique Ribeiro <henrichrb@gmail.com>
2023-09-07 22:02:39 -0300

    feat: Handling multiples request in parallel

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,10 +1,18 @@
 
 const pokeApi = {}
 
-pokeApi.getPokemons = (ofset = 0, limit = 10) => {
-    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+pokeApi.getPokemondetail = (pokemon) => {
+    return fetch(pokemon.url)
+    .then((response) => response.json())
+}
+
+pokeApi.getPokemons = (offset = 0, limit = 10) => {
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
     return fetch(url)
         .then((response) => response.json())
-        .then((response) => response.results)
-        .catch((error) => console.error(error))
+        .then((jsonBody) => jsonBody.results)
+        .then((pokemons) => pokemons.map(pokeApi.getPokemondetail))
+        .then((detailRequests) => Promise.all(detailRequests))
+        .then((pokemonsDetails) => pokemonsDetails)
 }
