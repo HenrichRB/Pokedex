e3e2de4ecb787a5421e73d96fa59e89ef52173f6
Henrich Ribeiro <henrichrb@gmail.com>
2024-01-24 20:07:42 -0300

    feat: Creating mechanism to limit in the first generation of Pokémon

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,11 @@
 
 const pokemonList = document.getElementById('pokemonList');
 const loadMoreButton = document.getElementById('loadMoreButton')
-const limit = 5
+
+const maxRecords = 151
+const limit = 10
 let offset = 0
+
 
 
 function loadPokemonItens(offset, limit) {
@@ -30,5 +33,16 @@ loadPokemonItens(offset, limit)
 
 loadMoreButton.addEventListener('click', () => {
     offset += limit
-    loadPokemonItens(offset, limit)
+
+    const qtdRecordNextPage = offset + limit
+
+    if (qtdRecord => maxRecords) {
+        const newLimit = maxRecords - offset
+        loadPokemonItens(offset, newLimit)
+
+        loadMoreButton.parentElement.removeChild(loadMoreButton)
+    } else {
+        loadPokemonItens(offset, limit)
+    }
+
 })
